e357324de9c34d6baa2b93747de58ae0f33b4c07
David Jones <ziphrax@users.noreply.github.com>
2018-11-10 15:02:35 +0000

    Added drawClock function

--- clock_led.ino
+++ clock_led.ino
@@ -1,17 +1,22 @@
-// This #include statement was automatically added by the Particle IDE.
-#include <MQTT.h>
-
-// This #include statement was automatically added by the Particle IDE.
+//REF: https://github.com/jackw01/led-ring-clock/blob/master/led-ring-clock.ino
 #include <math.h>
+#include <MQTT.h>
 #include <neopixel.h>
 
 #define PIN D2
-#define NUM_LEDS 150
+#define NUM_LEDS 76
 #define PIXEL_TYPE WS2812B
+
+
+STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
 
 Adafruit_NeoPixel strip(NUM_LEDS, PIN, PIXEL_TYPE);
 
-MQTT client("192.168.1.20", 1883, callback);
+MQTT client("192.168.0.19", 1883, callback);
+
+int hourGlowWidth = 2;
+int secondGlowWidth = 1;
+int minuteGlowWidth = 2;
 
 void setup() {
     strip.begin();
@@ -22,15 +27,20 @@ void setup() {
     
     // publish/subscribe
     if (client.isConnected()) {
-        client.publish("test/rpi_1","photon connected");
-        client.subscribe("test/rpi_1");
+        client.publish("home","clock connected");
+        client.subscribe("home/clock");
     }
 }
 
 void loop() {
-    //meteorRain(0xff,0xff,0xff,10, 64, true, 20);
-    if (client.isConnected())
+    
+    if (client.isConnected()){
         client.loop();
+    }
+    
+    drawClock();
+    
+    Particle.process();
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -38,22 +48,106 @@ void callback(char* topic, byte* payload, unsigned int length) {
     memcpy(p, payload, length);
     p[length] = NULL;
     
+    String red = "red";
+    String green = "green";
+    String blue = "blue";
     
 
-    if (!strcmp(p, "RED")){
+    if (red.equalsIgnoreCase(p)){
         meteorRain(0xff,0x0,0x0,10, 64, true, 20);
     }
-    else if (!strcmp(p, "GREEN")){
+    else if (green.equalsIgnoreCase(p)){
         meteorRain(0x0,0xff,0x0,10, 64, true, 20);
     }
-    else if (!strcmp(p, "BLUE")){
+    else if (blue.equalsIgnoreCase(p)){
         meteorRain(0x0,0x00,0xff,10, 64, true, 20);
     }
     else{
-        meteorRain(0xff,0xff,0xff,10, 64, true, 20);
+        Colours(100);
+        Strobe(0x33, 0x33, 0x33, 5, 500, 20);
     }
         
     delay(1000);
+    
+    Particle.process();
+}
+
+void drawClock(){
+    int hour = hourPosition();
+    int min = minPosition();
+    int sec = secondPosition();
+    
+    setAll(0,0,0);
+    
+    for(float i = hour - hourGlowWidth; i<= hour+ hourGlowWidth; i++){
+        setPixel(i, 0xff,0xff,0xff);    
+    }
+    
+    for (float i = min - minuteGlowWidth; i <= min + minuteGlowWidth; i++) {
+		setPixel(i, 0x00,0x00,0xff); 
+	}
+    
+    for (float i = sec - secondGlowWidth; i <= sec + secondGlowWidth; i++) {
+		setPixel(i, 0xff,0x00,0xff); 
+	}
+	
+	showStrip();
+	
+	
+    
+	Particle.process();
+}
+
+float hourPosition() {
+	int hour;
+	int currentHour;
+	currentHour = Time.hour();
+	
+	int currentMin;
+	currentMin = Time.minute();
+	
+	if (currentHour > 12) hour = (currentHour - 12) * (NUM_LEDS / 12);
+    else hour = currentHour * (NUM_LEDS / 12);
+	return hour + mapFloat(currentMin, 0.0, 59.0, 0.0, (NUM_LEDS / 12.0) - 1.0);
+	
+}
+
+float minPosition(){
+    return mapFloat((float)Time.minute() + ((1.0 / 60.0) * (float)Time.second()), 0.0, 59.0, 0.0, (float)NUM_LEDS);
+}
+
+
+float secondPosition() {
+	return mapFloat(Time.second(), 0.0, 60.0, 0.0, (float)NUM_LEDS);
+}
+
+float mapFloat(float x, float inMin, float inMax, float outMin, float outMax) {
+	return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+void Colours(int SpeedDelay){
+    for (int color=0; color<255; color++) {
+      for (int i=0; i<NUM_LEDS; i++) {
+        strip.setPixelColor(i, Wheel(color));
+       }
+    strip.show();
+    delay(SpeedDelay);
+  }
+    
+    Particle.process();
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
 }
 
 void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay) {  
@@ -79,6 +173,21 @@ void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTra
     showStrip();
     delay(SpeedDelay);
   }
+  
+  Particle.process();
+}
+
+void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause){
+  for(int j = 0; j < StrobeCount; j++) {
+    setAll(red,green,blue);
+    showStrip();
+    delay(FlashDelay);
+    setAll(0,0,0);
+    showStrip();
+    delay(FlashDelay);
+  }
+ 
+ delay(EndPause);
 }
 
 void fadeToBlack(int ledNo, byte fadeValue) {
